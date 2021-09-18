2f6de6e734de64e7b554dc991b566c686a70127d
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2021-09-17 18:59:38 -0700

    Confine the player square to stay in bounds.

--- src/world/collide.c
+++ src/world/collide.c
@@ -6,14 +6,27 @@ WORLD_IMPLEMENT_COLLIDE()
 #include <flecs_components_geometry.h>
 
 // TODO: Flesh this out some more with real tiles.
-WORLD_DEF_SYS(collide_actor_to_tile, EcsPosition2, EcsVelocity2) {
-  EcsPosition2 *p = ecs_term(it, EcsPosition2, 1);
-  EcsVelocity2 *v = ecs_term(it, EcsVelocity2, 2);
+WORLD_DEF_SYS(collide_actor_to_tile, EcsPosition2, EcsVelocity2, EcsSquare) {
+  EcsPosition2* p = ecs_term(it, EcsPosition2, 1);
+  EcsVelocity2* v = ecs_term(it, EcsVelocity2, 2);
+  EcsSquare* square = ecs_term(it, EcsSquare, 3);
 
   for (int i = 0; i < it->count; i ++) {
+    float size = square[i].size;
     if (p[i].y > VIDEO_HEIGHT) {
       p[i].y = VIDEO_HEIGHT;
-      v[i].y = 0; // clear vertical velocity back to zero
+      v[i].y = 0;
+    } else if (p[i].y < size) {
+      p[i].y = size;
+      v[i].y = 0;
+    }
+    if (p[i].x > VIDEO_WIDTH) {
+      p[i].x = VIDEO_WIDTH;
+      v[i].x = 0;
+    }
+    if (p[i].x < size) {
+      p[i].x = size;
+      v[i].x = 0;
     }
   }
 }
